036879f6672fc7109e238476b030ed0fcc1b8453
hwa19 <hwa8019@gmail.com>
2022-12-18 18:22:19 +0900

    hwa19 추가해볼까 /추가

--- src/views/category2/Category2Menu1.vue
+++ src/views/category2/Category2Menu1.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>kyh-p002 / This is an Category2 Menu1 Page</h1>
-    <h2>hwa29 test</h2>
+    <h2>hwa29 test / hwa19 추가해볼까</h2>
     <h2>hwa29 test / hwa29 로컬폴더에서 추가하기 / hwa19 로컬폴더에서 추가하기</h2>
   </div>
 </template>
