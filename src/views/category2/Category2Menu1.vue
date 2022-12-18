7a6432264c24aaacdead211d1b15f24b0c26a3f6
hwa29 <hwa8019+1@gmail.com>
2022-12-18 17:46:04 +0900

    hwa29 로컬폴더에서 같은파일 수정 추가

--- src/views/category2/Category2Menu1.vue
+++ src/views/category2/Category2Menu1.vue
@@ -2,6 +2,6 @@
   <div class="about">
     <h1>kyh-p002 / This is an Category2 Menu1 Page</h1>
     <h2>hwa29 test</h2>
-    <h2>hwa29 test / hwa29 계정에서 추가하기</h2>
+    <h2>hwa29 test / hwa29 로컬폴더에서 추가하기</h2>
   </div>
 </template>
