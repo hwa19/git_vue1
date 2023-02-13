0d695d97a1b7cd1783c0e208f106d0aab2a74292
hwa19 <hwa8019@gmail.com>
2023-02-13 17:46:54 +0900

    카테고리 메뉴 / 추가 작업

--- src/views/category2/Category2Menu1.vue
+++ src/views/category2/Category2Menu1.vue
@@ -3,5 +3,6 @@
     <h1>kyh-p002 / This is an Category2 Menu1 Page</h1>
     <h2>hwa29 test / hwa19 추가해볼까 / hwa29 추가</h2>
     <h2>hwa29 test / hwa29 로컬 찐 폴더에서 추가하기 + hwa29 로컬폴더에서 추가하기 / hwa19 로컬폴더에서 추가하기</h2>
+    <p>new 추가연습</p>
   </div>
 </template>
