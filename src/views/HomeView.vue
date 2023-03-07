73815fabce1199b73d8bc5a80b2e37bdd41cc246
hwa19 <hwa8019@gmail.com>
2023-03-07 14:47:32 +0900

    aa7 수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App aa6" />
+    <HelloWorld msg="Welcome to Your Vue.js App aa7" />
   </div>
 </template>
 
