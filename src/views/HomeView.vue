9ca8609d3d46f1161267674eae44ae9bf8f29ce7
hwa19 <hwa8019@gmail.com>
2023-03-07 12:15:49 +0900

    aa4 변경

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App aa2" />
+    <HelloWorld msg="Welcome to Your Vue.js App aa4" />
   </div>
 </template>
 
